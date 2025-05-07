f0d230a95424813c0bbf1c54ab3b166930489043
中島浩二 <knaka0209@gmail.com>
2025-05-07 17:45:08 +0900

    up, 0507a3

--- mcp-cli-3/src/index.ts
+++ mcp-cli-3/src/index.ts
@@ -12,6 +12,7 @@ import { fileWriteCsv } from "./tools/fileWriteCsv";
 import { addItemCsv } from "./tools/addItemCsv";
 import { getWeatherInfo } from "./tools/getWeatherInfo";
 import { getSpreadSheet } from "./tools/getSpreadSheet";
+import { getSheetSearchUp } from "./tools/getSheetSearchUp";
 
 import { generateText, tool } from "ai";
 import { z } from "zod";
@@ -31,7 +32,7 @@ async function executeMcp(input: string) {
     tools: {
       getNumber, addTodo, saveWorkHour , getWorkList , addTask , 
       getTodoList , addItemPrice , sendMail , addItemMail , fileWriteCsv ,
-      addItemCsv , getWeatherInfo , getSpreadSheet ,
+      addItemCsv , getWeatherInfo , getSpreadSheet , getSheetSearchUp , 
     },
     maxSteps: 5,
     messages: [{ role: "user", content: input }],
